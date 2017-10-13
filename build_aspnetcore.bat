96b86a83b143a3c9dd91773097b0a0cbfab88ada
Julian Verdurmen <304NotModified@users.noreply.github.com>
2017-10-14 00:57:13 +0200

    Update build_aspnetcore.bat

--- build_aspnetcore.bat
+++ build_aspnetcore.bat
@@ -2,6 +2,8 @@ echo off
 
 rem update NLog.Web.AspNetCore.csproj for version number
 
+dotnet restore "examples/ASP.NET Core 2/Visual Studio 2017/ASP.NET Core 2 - VS2017/"
+
 cd NLog.Web.AspNetCore
 msbuild    /t:restore /t:build /p:configuration=release /verbosity:minimal
 cd ..
